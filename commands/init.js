5a00a0d26c7a9cade2e760f0ca01c977eefc1867
jmanly56 <36718956+jmanly56@users.noreply.github.com>
2023-04-28 17:48:02 -0700

    Create init command. Appears to be working well (#2)
    * Create init command. Appears to be working well
    
    * Editing comments.
    
    * Formatting and adding getID

--- commands/init.js
+++ commands/init.js
@@ -1,5 +1,21 @@
-const {SlashCommandBuilder} = require('discord.js');
+const {SlashCommandBuilder, GuildTextThreadManager} = require('discord.js');
 const {BossInfo} = require('../exports/constants.js');
+const {setAllIds, loadPosts, savePosts} = require('../lib/posts');
+
+/**
+ * Create a forum post for bossName.
+ * @param {String} bossName
+ * @param {GuildTextThreadManager} channel
+ * @returns The id of the created thread channel.
+ */
+async function createPost(bossName, channel) {
+    const thread = await channel.threads.create({
+        name: bossName,
+        reason: 'Boss timer.',
+        message: 'World boss timers:',
+    });
+    return thread.id;
+}
 
 // Set up the command for deployment.
 module.exports = {
@@ -8,33 +24,43 @@ module.exports = {
         .setDescription('Initialize the forum posts.')
         .setDefaultMemberPermissions('32') // Member has manage server permissions.
         .setDMPermission(false)
-        .addChannelOption((option) => {
+        .addChannelOption((option) =>
             option
                 .setName('channel')
                 .setDescription('The channel to create the timers under.')
-                .setRequired(true);
-        }),
+                .setRequired(true)
+        ),
 
     // Execute the command.
     async execute(interaction) {
+        loadPosts();
         try {
-            //TODO: Create a function to check if they exist, otherwise create and store ID.
             await interaction.deferReply({ephemeral: true});
             const targetChannel = await interaction.options.getChannel(
                 'channel'
             );
-            const bossNames = Object.keys(BossInfo);
+            const bossNames = Object.keys(BossInfo).reverse();
+            console.log(bossNames);
             var ids = {};
             for (var key in bossNames) {
-                const t = await targetChannel.threads.cache.find(x => x.name === key);
-                var tID;
-                if (t === 'undefined') {
-                    //TODO: Create thread
+                key = bossNames[key];
+                const t = await targetChannel.threads.cache.find(
+                    (x) => x.name === key
+                );
+                if (typeof t === 'undefined') {
+                    ids[key] = await createPost(key, targetChannel);
+                    console.log(ids[key]);
                 } else {
                     ids[key] = t.id;
                 }
             }
+            setAllIds(ids);
+            savePosts();
+            await interaction.editReply('Sucessfully initialized.');
         } catch (error) {
+            await interaction.editReply(
+                'There was an error processing the request, contact moderator.'
+            );
             return console.log(error);
         }
     },
