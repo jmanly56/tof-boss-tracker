b5d6942cf0e2fe7fe1b5a8be6ab5e90145f654ea
Jonny <faane784@gmail.com>
2023-04-28 17:43:24 -0700

    Formatting and adding getID

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,14 @@
-const { ActivityType, Events } = require('discord.js');
+const {ActivityType, Events} = require('discord.js');
 
 module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	async execute(client) {
-		// Set activity
-		client.user.setActivity('Tower of Fantasy', { type: ActivityType.Playing });
-		// Announce that the BOT is ready.
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
+    name: Events.ClientReady,
+    once: true,
+    async execute(client) {
+        // Set activity
+        client.user.setActivity('Tower of Fantasy', {
+            type: ActivityType.Playing,
+        });
+        // Announce that the BOT is ready.
+        console.log(`Ready! Logged in as ${client.user.tag}`);
+    },
 };
